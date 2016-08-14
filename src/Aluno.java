db24d374529fb3c629977d6bc9fc4e7371386f7a
unip <rafaelclaus331@gmail.com>
2016-08-13 23:17:29 -0300

    initial commit

---
+++ src/Aluno.java
@@ -0,0 +1,23 @@
+
+public class Aluno {
+
+	public static void main(String[] args) {
+
+		String nome = "Rafael";
+		String sobrenome = "Claus";
+		int idade = 27;
+		
+		if (idade > 18) {
+			System.out.println("O Aluno " + nome + " é Maior de idade.");
+		}else {
+			System.out.println("O Aluno " + nome + " é Menor de idade.");
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
